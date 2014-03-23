6169a36921c28063568f5e3a877541c0d8cb07e9
dark-panda <dark.panda@gmail.com>
2014-03-23 13:46:34 -0400

    Change COPYING to MIT-LICENSE.

--- ext/jseal.h
+++ ext/jseal.h
@@ -2,7 +2,7 @@
 /*
  * Copyright (c) 2002-2014 J Smith <dark.panda@gmail.com>
  * Crypto++ copyright (c) 1995-2013 Wei Dai
- * See COPYING for the extact license
+ * See MIT-LICENSE for the extact license
  */
 
 #ifndef __JSEAL_H__
