1655730a13f834c9402f8283f6ee6c48a3659ec0
dark-panda <dark.panda@gmail.com>
2014-03-23 13:45:44 -0400

    Bump copyright year.

--- ext/jmars.h
+++ ext/jmars.h
@@ -1,6 +1,6 @@
 
 /*
- * Copyright (c) 2002-2013 J Smith <dark.panda@gmail.com>
+ * Copyright (c) 2002-2014 J Smith <dark.panda@gmail.com>
  * Crypto++ copyright (c) 1995-2013 Wei Dai
  * See COPYING for the extact license
  */
